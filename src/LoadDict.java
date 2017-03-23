54ae8ea3719d99a95afb94603b545f9635e37e8c
Krokogator <michal26262@gmail.com>
2017-03-23 02:37:39 +0100

    Console testing

--- src/LoadDict.java
+++ src/LoadDict.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.math.BigInteger;
 
 /**
  * Created by micha on 22.03.2017.
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Dictionary dictionary = new Dictionary();
         Character[][] input = {{'a','b','e','f'},{'a','b','c','d'},{'a','b','c','d'},{'a','b','c','d'}};
 
@@ -10,6 +14,27 @@ public class Main {
         long elapsedTime = System.currentTimeMillis() - startTime;
         long elapsedSeconds = elapsedTime / 1000;
         System.out.println("Load Time: "+ elapsedTime + "ms" +"\n");
-        dictionary.testPacket("stęchlizna");
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String textinput="";
+        System.out.println("Podaj wyraz: ");
+        textinput = br.readLine();
+        while(!textinput.equals("exit")) {
+            startTime = System.currentTimeMillis();
+            dictionary.testPacket(textinput);
+            elapsedTime = System.currentTimeMillis() - startTime;
+            if(elapsedTime<1) {
+                System.out.println("Search Time: < 1ms" + "\n");
+            }
+            else
+            {
+                System.out.println("Search Time: " + elapsedTime + "ms" + "\n");
+            }
+            textinput = br.readLine();
+        }
+
+
+
     }
 }
